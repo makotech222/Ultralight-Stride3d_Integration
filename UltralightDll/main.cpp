954a6f26a87c602bcc5f899a23ff98fd1026de13
Anon <makotech222@gmail.com>
2019-07-06 22:13:54 -0500

    JS functionality

--- UltralightDll/main.cpp
+++ UltralightDll/main.cpp
@@ -13,12 +13,15 @@
 #include <AppCore/JSHelpers.h>
 
 using namespace ultralight;
-using namespace std; 
+using namespace std;
+typedef void(__stdcall* CSharpCallBack)(const char*, const char*);
 
-class UltralightApp {
+class UltralightApp : public LoadListener {
 private:
 	RefPtr<Renderer> _renderer;
 	map<int, RefPtr<View>> _views;
+	bool _isReady = false;
+	map<std::string, CSharpCallBack> _callbacks;
 	int _id;
 
 public:
@@ -33,14 +36,22 @@ public:
 
 	virtual ~UltralightApp() {
 		_views.clear();
+		_callbacks.clear();
 		_renderer = nullptr;
 	}
 
+	virtual void OnDOMReady(View* caller) override
+	{
+		_isReady = true;
+	}
+
 	int CreateView(int width, int height, const char* htmlPath) {
+		_isReady = false;
 		auto view = _renderer->CreateView(width, height, true);
+		view->set_load_listener(this);
 		auto url = ultralight::String::String(htmlPath);
 		view->LoadURL(url);
-		while (view->is_loading() == false)
+		while (view->is_loading() == false || _isReady == false)
 			this->Update();
 		_views.insert(make_pair(_id++, view));
 		return _id;
@@ -83,16 +94,53 @@ public:
 		JSContextRef myContext = _views[viewId]->js_context();
 		SetJSContext(myContext);
 		auto a = JSEval(jsFunction);
-		//return String(.ToString()).utf16().udata();
 	}
-	//void CallJavascriptFunction(int viewId, const char* jsFunctionName, const JSArgs &args) {
-	//	JSObject global = JSGlobalObject();
-	//	JSFunction jsfunc = global[jsFunctionName];
-	//	if (jsfunc.IsValid())
-	//	{
-	//		JSValue result = jsfunc(args);
-	//	}
-	//}
+
+	void ExecuteJSFunction(int viewId, const char* function, const char* json) {
+		JSContextRef myContext = _views[viewId]->js_context();
+		SetJSContext(myContext);
+
+		JSObject global = JSGlobalObject();
+		JSFunction func = global[function];
+		if (func.IsValid()) {
+			JSValue result = func({ json });
+		}
+	}
+
+	void WriteToFile(std::string text) {
+		ofstream myfile;
+		myfile.open("example.txt");
+		myfile << text;
+		myfile.close();
+	}
+	void JSCallbackFunction(const JSObject& thisObject, const JSArgs& args, std::string funcName)
+	{
+		auto callback = _callbacks[funcName];
+		std::string argJson = "{";
+		for (size_t i = 0; i < args.size(); i++) {
+			const char* val = ultralight::String(args[i].ToString()).utf8().data();
+			char name = (char)('a' + i);
+			argJson += '"';
+			argJson += name;
+			argJson += '"';
+			argJson += ":";
+			argJson += '"';
+			argJson += val;
+			argJson += '"';
+			argJson += ',';
+		}
+		argJson += '}';
+		callback(funcName.c_str(), argJson.c_str());
+	}
+
+	void RegisterEventHandler(int viewId, const char* funcName, CSharpCallBack func) {
+		JSContextRef myContext = _views[viewId]->js_context();
+		SetJSContext(myContext);
+		JSObject global = JSGlobalObject();
+		_callbacks.insert(make_pair(std::string(funcName), func));
+
+		global[funcName] = (JSCallback)std::bind(&UltralightApp::JSCallbackFunction, this, std::placeholders::_1, std::placeholders::_2, std::string(funcName));
+	}
 	void DisposeView(int viewId) {
 		auto view = _views[viewId];
 		view->Release();
@@ -101,7 +149,6 @@ public:
 	}
 
 	RefPtr<Bitmap> GetViewBitmap(int viewId) {
-
 		auto view = _views[viewId];
 		return view->bitmap();
 	}
@@ -135,7 +182,7 @@ extern "C"
 	{
 		_app->ReloadView(viewId, htmlPath);
 	}
-	
+
 	__declspec(dllexport) void GetViewBitmap(int viewId, unsigned char** data, int* size)
 	{
 		auto bitmap = _app->GetViewBitmap(viewId);
@@ -158,6 +205,14 @@ extern "C"
 	__declspec(dllexport) void JavascriptEval(int viewId, const char* jsFunction)
 	{
 		_app->JavascriptEval(viewId, jsFunction);
+	}
+	__declspec(dllexport) void ExecuteJSFunction(int viewId, const char* function, const char* json)
+	{
+		_app->ExecuteJSFunction(viewId, function, json);
+	}
+	__declspec(dllexport) void RegisterEventHandler(int viewId, const char* funcName, CSharpCallBack func)
+	{
+		_app->RegisterEventHandler(viewId, funcName, func);
 	}
 	__declspec(dllexport) void DisposeView(int viewId)
 	{
